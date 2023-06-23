8acbe357c83991cd365773d778f8db2c3a739309
MatheusRodriguesBezerra <matheusrodbez@gmail.com>
2023-06-23 14:29:25 -0300

    refazendo a folha 7

---
+++ folha7/index.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <ctype.h>
+#include <stdlib.h>
+
+// 7.1
+int verifiy(int v[], int na, int n){
+    for(int i = 0; i <= na; i++){
+        if(v[i] == n){ 
+            return 1;
+        }       
+    }
+    return 0;
+}
+
+int naoRepetidos(){
+    int n, atuais[100], atuaisi = 0;
+    scanf("%d", &n);
+    atuais[atuaisi] = n;
+    atuaisi++;
+    printf("%d ", n);
+    while(n != -1){
+        
+    }
+    
+    
+    
+}
+
+
+int main() {
+
+}
